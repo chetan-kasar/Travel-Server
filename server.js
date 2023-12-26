d0d7b8d18bebda4a9780024ba92fece54694008a
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-26 15:38:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const multer  = require('multer')
 const cors = require('cors');
 const app = express();
 
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 global.titleImageName = "";
 global.albumImages = {};
 
